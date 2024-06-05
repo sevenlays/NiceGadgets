58cd5ca059e9250196dcad6af7c1a5b9c989a8b1
Dmytro Koriahin <Dkoryahin@gmail.com>
2024-06-06 01:11:28 +0300

    upgrade App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import { AppRouter } from './router';
 import { Test } from './components/test/Test';
 
 import './App.scss';
+import { Footer } from './components/FooterComponents/Footer/Footer';
+import { Header } from './components/HeaderComponents/Header/Header';
+import { MobileMenu } from './components/MenuComponents/MobileMenu/MobileMenu';
 
 export const App = () => {
   return (
     <div className="App">
+      <MobileMenu />
+      <Header />
       <AppRouter />
 
       <Test />
+      <Footer />
     </div>
   );
 };
