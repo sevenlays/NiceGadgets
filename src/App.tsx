a7fbcbabb35ebef7d022fcea2f2d2c3891aafe4a
Dmytro Koriahin <Dkoryahin@gmail.com>
2024-06-05 18:08:26 +0300

    add app

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,6 @@ import { AppRouter } from './router';
 import { Test } from './components/test/Test';
 
 import './App.scss';
-import { Test } from './components/test/Test';
-import { Header } from './components/HeaderComponents/Header/Header';
-import { Footer } from './components/FooterComponents/Footer/Footer';
-import { MobileMenu } from './components/MenuComponents/MobileMenu/MobileMenu';
 
 export const App = () => {
   return (
