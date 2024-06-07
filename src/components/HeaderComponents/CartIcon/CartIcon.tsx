01372a367cb637808221061a9a278c71f69db9eb
Vladislav <7evenLays@gmail.com>
2024-06-07 17:35:36 +0300

    added active classes to header links

--- src/components/HeaderComponents/CartIcon/CartIcon.tsx
+++ src/components/HeaderComponents/CartIcon/CartIcon.tsx
@@ -1,23 +1,28 @@
 import cn from 'classnames';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Button } from '../../../UI';
 import icon from '../../../assets/icons/Shopping bag.svg';
 import styles from './CartIcon.module.scss';
 import { PATHS } from '../../../constants';
 
-export const CartIcon = () => (
-  <div
-    className={cn(styles.icon, {
-      [styles.icon_active]: false,
-    })}
-  >
-    <NavLink
-      to={`${PATHS.CART}`}
-      style={{ width: '100%', boxSizing: 'border-box' }}
+export const CartIcon = () => {
+  const { pathname } = useLocation();
+  const isActiveClass = pathname.includes('cart');
+
+  return (
+    <div
+      className={cn(styles.icon, {
+        [styles.icon_active]: isActiveClass,
+      })}
     >
-      <Button type="icon" size={{ width: 100, height: 100 }} measure="%">
-        <img src={icon} alt="burger-icon" className={styles.icon__img} />
-      </Button>
-    </NavLink>
-  </div>
-);
+      <NavLink
+        to={`${PATHS.CART}`}
+        style={{ width: '100%', boxSizing: 'border-box' }}
+      >
+        <Button type="icon" size={{ width: 100, height: 100 }} measure="%">
+          <img src={icon} alt="burger-icon" className={styles.icon__img} />
+        </Button>
+      </NavLink>
+    </div>
+  );
+};
--- src/components/HeaderComponents/FavourIcon/FavourIcon.tsx
+++ src/components/HeaderComponents/FavourIcon/FavourIcon.tsx
@@ -1,23 +1,28 @@
 import icon from '../../../assets/icons/Favourites.svg';
 import styles from './FavourIcon.module.scss';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Button } from '../../../UI';
 import cn from 'classnames';
 import { PATHS } from '../../../constants';
 
-export const FavourIcon = () => (
-  <div
-    className={cn(styles.icon, {
-      [styles.icon_active]: false,
-    })}
-  >
-    <NavLink
-      to={`${PATHS.FAVOURITES}`}
-      style={{ width: '100%', boxSizing: 'border-box' }}
+export const FavourIcon = () => {
+  const { pathname } = useLocation();
+  const isActiveClass = pathname.includes('favourites');
+
+  return (
+    <div
+      className={cn(styles.icon, {
+        [styles.icon_active]: isActiveClass,
+      })}
     >
-      <Button type="icon" size={{ width: 100, height: 100 }} measure="%">
-        <img src={icon} alt="burger-icon" className={styles.icon__img} />
-      </Button>
-    </NavLink>
-  </div>
-);
+      <NavLink
+        to={`${PATHS.FAVOURITES}`}
+        style={{ width: '100%', boxSizing: 'border-box' }}
+      >
+        <Button type="icon" size={{ width: 100, height: 100 }} measure="%">
+          <img src={icon} alt="burger-icon" className={styles.icon__img} />
+        </Button>
+      </NavLink>
+    </div>
+  );
+};
