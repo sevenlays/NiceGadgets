2cca344b2358c8c2f7a74f625fd761e0d9194d65
ordila <ordila99@gmail.com>
2024-06-07 15:36:08 +0300

    com anoth

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Test } from './components/test/Test';
 import './App.scss';
 
 import { HomeSlider } from './components/HomeSlider/HomeSlider';
-// import { VariableWidth } from './components/SliderItems/SliderItems';
 
 export const App = () => {
   return (
@@ -21,8 +20,6 @@ export const App = () => {
 
       <div>3</div>
       <div>4</div>
-
-      {/* <VariableWidth /> */}
     </div>
   );
 };
