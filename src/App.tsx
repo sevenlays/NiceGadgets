a23b14738d28d9439f946d2fd54e8714b6987f9b
marchAlbion <marchAlbion@gmail.com>
2024-06-13 19:43:10 +0300

    Theme toggler component + language theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import { useEffect } from 'react';
 import './App.scss';
 import { fetchProducts } from './redux';
 import { useAppDispatch } from './hooks/useAppDispatch';
+import { useSelector } from 'react-redux';
+import { selectTheme } from './redux';
 
 export const App = () => {
   const dispatch = useAppDispatch();
+
+  const theme = useSelector(selectTheme);
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
   return (
-    <div className="App" data-theme="Original-dark">
+    <div className="App" data-theme={theme}>
       <AppRouter />
     </div>
   );
