700b50466ac3a903dd4780869deb20a11daba268
marchAlbion <marchAlbion@gmail.com>
2024-06-15 11:00:06 +0300

    Empty carts placeholdres

---
+++ src/pages/Favourites/EmptyFavourites/Emptyfav.tsx
@@ -0,0 +1,9 @@
+import style from './Emptyfav.module.scss';
+
+export const EmptyFav = () => {
+  return (
+    <div className={style.cart}>
+      <div className={style.cart__cat}></div>
+    </div>
+  );
+};
