d3bb0a2e1808087659d9399c22a4f52cab9a576b
Vladislav <7evenLays@gmail.com>
2024-06-15 15:44:38 +0300

    fixed active class on mob menu

--- src/components/HeaderComponents/MobileMenu/MobileMenu.tsx
+++ src/components/HeaderComponents/MobileMenu/MobileMenu.tsx
@@ -7,7 +7,7 @@ import { ReactComponent as FavouriteIcon } from '../../../assets/icons/Favourite
 import { ReactComponent as CartIcon } from '../../../assets/icons/Shopping bag.svg';
 import cn from 'classnames';
 import { Nav } from '../Nav/Nav';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { PATHS } from '../../../constants';
 
 type Props = {
@@ -24,6 +24,10 @@ export const MobileMenu: React.FC<Props> = ({
   // const handleCloseMenu = () => {
   //   setShowMenu(false);
   // };
+
+  const { pathname } = useLocation();
+  const isActiveFavorites = pathname.includes('favourites');
+  const isActiveCart = pathname.includes('cart');
 
   return (
     <aside
@@ -43,7 +47,7 @@ export const MobileMenu: React.FC<Props> = ({
       <div className={styles.footer}>
         <div
           className={cn(styles.icon__large, {
-            [styles.icon__large_active]: true,
+            [styles.icon__large_active]: isActiveFavorites,
           })}
         >
           <NavLink
@@ -63,7 +67,7 @@ export const MobileMenu: React.FC<Props> = ({
         </div>
         <div
           className={cn(styles.icon__large, {
-            [styles.icon__large_active]: false,
+            [styles.icon__large_active]: isActiveCart,
           })}
         >
           <NavLink
