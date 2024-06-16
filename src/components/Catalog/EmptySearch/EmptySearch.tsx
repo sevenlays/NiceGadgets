266018c645475174f50418b38cf318b96342800f
Oleksii Dunchyk <oleksiidunchik@gmail.com>
2024-06-16 08:44:17 +0300

    fix searchNotFound image to correctly work with different themes

--- src/components/Catalog/EmptySearch/EmptySearch.tsx
+++ src/components/Catalog/EmptySearch/EmptySearch.tsx
@@ -1,6 +1,9 @@
 import style from './EmptySearch.module.scss';
-import EmptySearchCat from '../../../assets/Search_not_found.png';
 
 export const EmptySearch = () => {
-  return <img src={EmptySearchCat} alt="123" className={style.emptySearch} />;
+  return (
+    <div className={style.emptySearchContainer}>
+      <div className={style.emptySearch}></div>
+    </div>
+  );
 };
